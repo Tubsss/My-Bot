87625ad0f0ef3a316a047254c2c3e32a8b76765f
Glitch (alpine-ionized-handstand) <none>
2021-09-08 03:07:08 +0000

    🌌🎅 Checkpoint ./config.js:35877557/6

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `t.`;//your prefix for bot
+exports.Prefix = `t!`;//your prefix for bot
 exports.Token = `ODc4MDYxODM3Mjk4NTk3OTM5.YR7szw.z7PRaMOzhLPwTpsbaUghE0T9Anc`;//your token 
 exports.Color = `BLUE`;//color   of embed
